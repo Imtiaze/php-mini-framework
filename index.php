1912bd4f68e9e54e70a0f26927b0d7c9b1b9c3e3
Imtiaze <ahammedimtiaze78@gmail.com>
2026-06-26 11:51:43 +0600

    Create mysql database and switch to  PDO

--- index.php
+++ index.php
@@ -2,4 +2,18 @@
 
 require 'functions.php';
 
+$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn, 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+
 require 'routes.php';
