a2883d8cdc0732a4bd929ccbbb5ca71573b9555e
Imtiaze <ahammedimtiaze78@gmail.com>
2026-06-26 13:42:52 +0600

    Database class created and PDO refactored

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php 
 
 require 'functions.php';
+require 'Database.php';
 
-$dsn = "mysql:host=127.0.0.1;port=3306;dbname=myapp;charset=utf8mb4";
 
-$pdo = new PDO($dsn, 'root', '');
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$config = require('config.php');
 
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
 
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+
 
 foreach ($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
 }
+
 
 require 'routes.php';
